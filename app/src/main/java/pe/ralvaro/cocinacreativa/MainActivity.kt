c80d30fddde7d8fc753a46b756412b875de0bea1
Alvaro Ramirez <ralvaro.dev@gmail.com>
2023-07-07 14:08:43 -0500

    update: fragment view to fragment container view and material 3

--- app/src/main/java/pe/ralvaro/cocinacreativa/MainActivity.kt
+++ app/src/main/java/pe/ralvaro/cocinacreativa/MainActivity.kt
@@ -3,7 +3,9 @@ package pe.ralvaro.cocinacreativa
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,15 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_explore, R.id.navigation_favourites
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
         navView.setupWithNavController(navController)
+
     }
 }
